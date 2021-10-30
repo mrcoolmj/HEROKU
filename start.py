74957f664107f8e30adffb058e6baa1e18631bfc
mrcoolmj <90083311+mrcoolmj@users.noreply.github.com>
2021-10-30 08:58:21 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/mrcoolmj/catuserbot && cd catuserbot && python3 -m userbot")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
